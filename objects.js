1dd7c4020ac324ab07741b4d876a28db08c12ff4
Christina Park <christinapark0117@gmail.com>
2019-07-30 04:26:27 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,8 +1,7 @@
 var playlist = { Queens: 'Radio Gaga', Beatles: 'Hey, Jude'}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.songTitle = 
-  return playlist
+  
 }
 
 function removeFromPlaylist (playlist, artistName) {
