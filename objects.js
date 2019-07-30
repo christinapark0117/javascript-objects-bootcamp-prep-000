040fdc117277b0c4d6cb7e32fe70717cb575167e
Christina Park <christinapark0117@gmail.com>
2019-07-30 04:30:04 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = { Queens: 'Radio Gaga', Beatles: 'Hey, Jude'}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist['Beatles']=
+  playlist['Beatles']='My Bloody Valentine'
   return playlist
 }
 
